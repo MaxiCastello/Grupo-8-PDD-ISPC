4255a748e9f3a817a7faec4bbaf329617b6fecf6
pevescarol <carol.peves@gmail.com>
2023-06-28 21:34:34 -0300

    fix names

--- PDD.py
+++ PDD.py
@@ -20,18 +20,18 @@ df.describe()
 # DISTRIBUCIÓN DE LA EDAD:
 edad_dist = (20, 5)
 edad_dist, ax2 = plt.subplots(figsize=edad_dist)
-ax2 = sns.countplot(x=df['Age'])
+ax2 = sns.countplot(x=df['age'])
 plt.show()
 ## La edad en el dataset va de 28 a 77, siendo el grupo de 50 a 60 el rango etáreo que más cantidad de personas contiene.
 
 # DISTRIBUCION DEL COLESTEROL: 
-sns.displot(x=df['Cholesterol'], bins=30)
-sns.show()
+sns.displot(x=df['chol'], bins=30)
+plt.show()
 ## Se ve que hay muchisimos registros con valor igual a 0 para ésta variable. Habria que analizar si hay pacientes con colesterol igual a 0, o si es un error del dataset
 
 # CANTIDAD DE PERSONAS QUE PRESENTAN COLESTEROL 0
-colesterol_0 = df[df['Cholesterol']==0]
-print(" La cantidad de pacientes que presentan niveles de colesterol 0 son: ",colesterol_0.Cholesterol.count())
+colesterol_0 = df[df['chol']==0]
+print(" La cantidad de pacientes que presentan niveles de colesterol 0 son: ",colesterol_0.chol.count())
 
 
 # PROPORCIÓN DEL GENERO
