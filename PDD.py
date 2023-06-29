ed8a5729d24d3b07afa6ef7780ff77e0a869217d
pevescarol <carol.peves@gmail.com>
2023-06-28 22:41:50 -0300

    add gender distribution according to target

--- PDD.py
+++ PDD.py
@@ -41,3 +41,36 @@ plt.title('Proporción de género')
 plt.show()
 # print(gender_counts)
 ## De los pacientes a quienes se les realizó el estudio, el 31.7%, es decir 96 pacientes eran mujeres y el 68.3%, corrspondiente a 207 pacientes eran hombres.
+
+
+# DISTRIBUCIÓN DE GÉNERO ENTRE PACIENTES CON ENFERMEDAD Y SIN ENFERMEDAD :
+
+# Reemplazar los valores de la columna 'Sex' con etiquetas personalizadas
+df['sex'] = df['sex'].replace({0: 'Mujeres', 1: 'Hombres'})
+
+# Calcular la distribución de género para cada valor de target
+gender_target_distribution = df.groupby(['sex', 'target']).size().reset_index(name='Count')
+
+# Mapear los valores de target a las categorías "Enfermo" y "No Enfermo"
+gender_target_distribution['target'] = gender_target_distribution['target'].map({0: 'Enfermo', 1: 'No Enfermo'})
+
+# Crear el gráfico de barras para la distribución de género
+plt.figure(figsize=(8, 5))
+sns.barplot(x='sex', y='Count', hue='target', data=gender_target_distribution, palette='Set2')
+
+# Agregar etiquetas a las barras
+for p in plt.gca().patches:
+    height = p.get_height()
+    plt.gca().annotate(f"{height}", (p.get_x() + p.get_width() / 2, height),
+        ha='center', va='bottom')
+
+# Configurar título y etiquetas de ejes
+plt.title('Distribución de Género según el Estado de Enfermedad')
+plt.xlabel('Género')
+plt.ylabel('Cantidad')
+
+plt.show()
+
+'''
+Podemos observar que entre los pacientes con enfermedad, los hombres tienen una mayor proporción en comparación con las mujeres.
+Es decir, según el conjunto de datos analizado, hay una mayor prevalencia de enfermedad cardíaca entre los hombres en comparación con las mujeres.'''
